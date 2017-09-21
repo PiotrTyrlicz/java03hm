31730f86f47d8b200a36b8999fbcd0d86e89b349
Piotr Tyrlicz <tyrlicz@wp.pl>
2017-09-21 09:36:32 +0200

    "Poprawka drugiego konstruktora w Device.java"

--- src/Device.java
+++ src/Device.java
@@ -4,19 +4,17 @@ public class Device {
     public String type;
     public double price;
 
-    Device (String code, String producer, String type, double price){
-        this.code=code;
-        this.producer=producer;
-        this.type=type;
-        this.price=price;
-
-    }
     Device (String code, String producer, String type){
         this.code=code;
         this.producer=producer;
         this.type=type;
-
     }
+
+    Device (String code, String producer, String type, double price) {
+        this(code, producer, type);
+        this.price = price;
+    }
+
     public String getInfo(){
         return code +" "+ producer + " " + type+ " " + price;
     }
